6f27559cba9f555b8dfea74dba3698e9265707ef
Max <maximomacchi@gmail.com>
2020-11-25 10:41:09 -0800

    Date boundaries and err catching added to DatePicker

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,18 +1,40 @@
-import React from 'react';
+// Simple DatePicker component which modifies date of date prop passed in
+//
+// Props:
+// date: Date in ISO string format (e.g. '2020-01-01')
+// changeDate: Function to change the value of date from its parent component
 
-function DatePicker({ date = new Date().toISOString(), changeDate }) {
+import React, { useState } from 'react';
+
+function DatePicker({ date, changeDate, min, max }) {
+  const [err, setErr] = useState(null);
+
   const handleChange = (e) => {
-    changeDate(e.target.value);
+    let newDate = new Date(e.target.value);
+    if (min && max) {
+      if (newDate >= min && newDate <= max) {
+        setErr(null);
+        changeDate(e.target.value);
+      } else {
+        setErr('Date must be within defined range');
+      }
+    }
   };
 
   return (
-    <form onSubmit={handleChange}>
-      <input
-        type="date"
-        value={date}
-        onChange={(e) => changeDate(e.target.value)}
-      ></input>
-    </form>
+    <div>
+      <form className="datePickerForm">
+        <label>Date: </label>
+        <input
+          className="dateInput"
+          type="date"
+          value={date}
+          required
+          onChange={handleChange}
+        ></input>
+      </form>
+      <div className="errorMsg">{err}</div>
+    </div>
   );
 }
 
