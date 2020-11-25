402a5c56e893976fea1500bf65f7d210d5070b14
Max <maximomacchi@gmail.com>
2020-11-25 10:45:55 -0800

    Added option for custom column names

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,21 @@
 // Table component used to show data
+//
+// Props:
+// cols: Array of columns (e.g. [col1, col2])
+// data: Array of objects consisting of col: value format
+// (e.g. [{col1: val1, col2: val2}])
 
 import React from 'react';
 
-function Table({ cols, data }) {
-  const renderCols = () => cols.map((col) => <th key={col}>{col}</th>);
+function Table({ cols, data, colNames }) {
+  const renderCols = () =>
+    cols.map((col) => {
+      if (colNames && colNames[col]) {
+        return <th key={col}>{colNames[col]}</th>;
+      } else {
+        return <th key={col}>{col}</th>;
+      }
+    });
 
   const renderRows = () => {
     return data.map((row, index) => {
@@ -17,16 +29,19 @@ function Table({ cols, data }) {
     });
   };
 
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>{renderCols()}</tr>
-        </thead>
-        <tbody>{renderRows()}</tbody>
-      </table>
-    </div>
-  );
+  if (cols.length && data.length) {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>{renderCols()}</tr>
+          </thead>
+          <tbody>{renderRows()}</tbody>
+        </table>
+      </div>
+    );
+  }
+  return null;
 }
 
 export default Table;
