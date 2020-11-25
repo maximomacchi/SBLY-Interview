0779e631648efbfdb25dc4267f18203245251c14
Max <maximomacchi@gmail.com>
2020-11-25 10:46:11 -0800

    Created SBLY_API module to replace useSBLY_API hook

---
+++ src/utilities/SBLY_API.js
@@ -0,0 +1,41 @@
+const fetchAdInsights = async (start, end) => {
+  let adInsights = {};
+  let curDate = new Date(start);
+  while (curDate <= end) {
+    let curDateISOString = curDate.toISOString().substring(0, 10);
+    const result = await fetch(
+      'https://interview-api.sbly.com/ad-insights?date=' +
+        curDateISOString +
+        '&metrics=spend,revenue,impressions,clicks',
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          Authorization: 'Bearer SHAREABLY_SECRET_TOKEN',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => data);
+    adInsights[curDate.toISOString().substring(0, 10)] = result;
+    curDate.setDate(curDate.getDate() + 1);
+  }
+  return adInsights;
+};
+
+const fetchAdBudget = async (id) => {
+  const result = await fetch('https://interview-api.sbly.com/ad/' + id, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      Authorization: 'Bearer SHAREABLY_SECRET_TOKEN',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+  return result;
+};
+
+export { fetchAdInsights, fetchAdBudget };
