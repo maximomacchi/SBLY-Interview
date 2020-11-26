d199f434aa00b08af11626635237342c28a0ed82
Max <maximomacchi@gmail.com>
2020-11-25 23:16:51 -0800

    Wrote README. Changed port used to serve webpack bundle

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# SBLY-Interview
+# Maximo Macchi - SBLY Interview
+
+## Running the app
+
+1. At root of project directory, run the command `npm install`
+2. Run the command `npm start`. This will serve the app locally at
+   (http://localhost:8000/). If desired, the port can be changed by editing line
+   34 in `webpack.config.js` in the root directory.
+
+**Note:** Please disable any ad blocker extensions as this can cause CORS issues
+when fetching API data
+
+## Sources Used
+
+[Jedai Saboteur Starter Guide](https://blog.usejournal.com/creating-a-react-app-from-scratch-f3c693b84658)
+
+Used to assist with setting up Babel and Webpack correctly
+
+[Fetch documentation](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch)
+
+Used to assist with writing syntax for fetch() functions within app
+
+[W3 Color Theme Generator](https://www.w3schools.com/w3css/w3css_color_generator.asp)
+
+Used to assist with styling app and picking colors that complement each other well
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.js',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
-  resolve: { 
-    extensions: ["*", ".js", ".jsx"],
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
     alias: {
       'react-dom': '@hot-loader/react-dom',
     },
   },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    contentBase: path.join(__dirname, 'public/'),
+    port: 8000,
+    publicPath: 'http://localhost:8000/dist/',
+    hotOnly: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
